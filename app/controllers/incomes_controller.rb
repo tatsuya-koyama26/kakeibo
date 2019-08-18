cde858b4d6c8aa4aac132574cf0b3c81810a67fe
tatsuya-koyam26 <koyama.tatsuya@icloud.com>
2019-08-18 20:28:00 +0900

    収入登録機能を追加

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,26 +1,38 @@
 class IncomesController < ApplicationController
   def index
     @incomes = Income.order("created_at ASC")
-	end
- 
+  end
+
   def show
     @income = Income.find(params[:id])
-	end
- 
+  end
+
   def new
-    @income = Income.new()
-	end
- 
+    @income = Income.new
+  end
+
   def edit
     @income = Income.find(params[:id])
-	end
- 
-	def create
-	end
- 
-	def update
-	end
- 
-	def destroy
-	end
+  end
+
+  def create
+    @income = Income.new(incomes_params)
+    if @income.save
+			redirect_to @income, notice: "収入科目を登録しました"
+		else
+			render "new"
+		end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def incomes_params
+    params.require(:income).permit(:name, :description)
+  end
 end
