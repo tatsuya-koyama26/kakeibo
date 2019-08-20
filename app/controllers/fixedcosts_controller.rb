e6ba832e6fce64b333f387e3b4733434a659f639
tatsuya-koyam26 <koyama.tatsuya@icloud.com>
2019-08-20 19:14:55 +0900

    Update fixedcosts_controller.rb

--- app/controllers/fixedcosts_controller.rb
+++ app/controllers/fixedcosts_controller.rb
@@ -1,2 +1,48 @@
 class FixedcostsController < ApplicationController
+  def index
+    @fixedcosts = Fixedcost.order("created_at ASC")
+  end
+
+  def show
+    @fixedcost = Fixedcost.find(params[:id])
+  end
+
+  def new
+    @fixedcost = Fixedcost.new
+  end
+
+  def edit
+    @fixedcost = Fixedcost.find(params[:id])
+  end
+
+  def create
+    @fixedcost = Fixedcost.new(fixedcosts_params)
+    if @fixedcost.save
+      redirect_to @fixedcost, notice: "収入科目を登録しました"
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @fixedcost = Fixedcost.find(params[:id])
+    @fixedcost.update(fixedcosts_params)
+    if @fixedcost.save
+      redirect_to @fixedcost, notice: "収入科目を編集しました"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @fixedcost = Fixedcost.find(params[:id])
+    @fixedcost.destroy
+    redirect_to :fixedcosts, notice: "科目を削除しました。"
+  end
+
+  private
+
+  def fixedcosts_params
+    params.require(:fixedcost).permit(:name, :description)
+  end
 end
